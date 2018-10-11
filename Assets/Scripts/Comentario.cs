8a2635b06fdd70490be60f377a50816f6507640c
Lui Rocha <luifr@usp.br>
2018-10-11 12:07:32 -0300

    random names

--- Assets/Scripts/Comentario.cs
+++ Assets/Scripts/Comentario.cs
@@ -19,16 +19,104 @@ public class Comentario : MonoBehaviour {
 				equipment.Add(t);				
 			}
 		}
+
+		nomes = new String [] {
+			"chickleslimy",
+			"slimycuckoo",
+			"andinfect",
+			"slimycheeked",
+			"stanhamslimy",
+			"popperinfect",
+			"infectcockney",
+			"slimyasprin",
+			"slimymiced",
+			"freeslimy",
+			"infectcastic",
+			"slimygratis",
+			"infectking",
+			"macaronslimy",
+			"slimyfaculae",
+			"jiggaslimy",
+			"slimyscornful",
+			"canoeslimy",
+			"hondoslimy",
+			"jickinginfect",
+			"slimyblue",
+			"slimymistassini",
+			"failingslimy",
+			"milveyinfect",
+			"radishinfect",
+			"infectbass",
+			"slimytheorem",
+			"infectpankey",
+			"infectsubtitles",
+			"infectdemurrage",
+			"fileslimy",
+			"arroweslimy",
+			"slimystagg",
+			"infectpork",
+			"infectmonitor",
+			"babiesslimy",
+			"kestrelslimy",
+			"apacheslimy",
+			"infectbanana",
+			"foofslimy",
+			"slimywaist",
+			"infectsquod",
+			"mediumslimy",
+			"ofslimy",
+			"infectskiing",
+			"slimydeather",
+			"oppositioninfect",
+			"feetyinfect",
+			"stoatinfect",
+			"infectahead",
+			"slimywax",
+			"pishslimy",
+			"slimygrapnel",
+			"gradingslimy",
+			"trentonslimy",
+			"shareslimy",
+			"windinginfect",
+			"expeditedsslinfect",
+			"infectscitch",
+			"disneyinfect",
+			"waldenslimy",
+			"fileslimy",
+			"biopsyslimy",
+			"infectgreaseproof",
+			"AloneChilled",
+			"Apenguinte",
+			"AprilPlatinum",
+			"BuggyCandy",
+			"Chasetson",
+			"CozyIan",
+			"Eventspart",
+			"Fallstone",
+			"Felinetlog",
+			"Giandatr",
+			"Healerte",
+			"Hosionst"
+		};
+
 	}
 
 	void Comentar(){
 		string s;
 		if(UnityEngine.Random.Range(0,100) > Atributos.rating){
+			Atributos.rep-=UnityEngine.Random.Range(1,Mathf.RoundToInt(Mathf.Log(Atributos.rep+1)*Mathf.Log(Atributos.rep+1))+1);
 			int i = getEquipmentToComment();
 			s = comentario[i][ UnityEngine.Random.Range(0,comentario[i].Length)  ];
 		}
 		else{
 			s = comentarioPositivo[UnityEngine.Random.Range(0,comentarioPositivo.Length)];
+			string nome = nomes[ UnityEngine.Random.Range(1,nomes.Length) ];
+			if(UnityEngine.Random.Range(1,100) > 70 ){
+				nome += "_gamer";
+			}
+			if(UnityEngine.Random.Range(1,100) > 60 ){
+				nome += UnityEngine.Random.Range(1,100000).ToString();
+			}
 		}
 		// printar s como comentario
 	}
