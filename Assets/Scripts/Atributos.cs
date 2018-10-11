80784a1b38cd3429bd88ca9e5a33e47bcd5ca5f6
Lui Rocha <luifr@usp.br>
2018-10-10 23:06:59 -0300

    scripts

---
+++ Assets/Scripts/Atributos.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Atributos : MonoBehaviour {
+
+	private int viewers;
+	private int rep;
+	private int dinheiro;
+
+
+	// Use this for initialization
+	void Start () {
+		dinheiro = 0;
+		rep = 0;
+		viewers = 2;
+	}
+	
+	void Comentario(){
+		
+	}
+
+	void CalculaViewers(){
+
+	}
+
+	void AtualizaAtributos(){
+
+	}
+
+	void ChecarVitoria(){
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+			
+	}
+}
---
+++ Assets/Scripts/Investimentos.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Investimentos : MonoBehaviour {
+
+	private int[] custo;
+	private float[] tempo;
+	private int currentTier;
+	private int maxTier;
+
+	// Use this for initialization
+	void Start () {
+		currentTier = 0;
+	}
+
+	void Investir(){
+		
+	}
+
+	public abstract void Efeito();
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
