be572d1e265c009efd07314f7a87f359d44dcf86
Unknown <will.leidan@gmail.com>
2018-10-12 13:42:55 -0300

    Animated Stream Prefab

--- Assets/Art/Vid/VideoControl.cs
+++ Assets/Art/Vid/VideoControl.cs
@@ -1,13 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class VideoControl : MonoBehaviour {
 
+	public Investimentos[] equips;
+	public int[] tests;
 	
+	[Space(10)]
+	public Image scenario;
+	public Sprite[] cams;
+
+	[Space(10)]
+	public Image microphone;
+	public Sprite[] mics;
+
+	[Space(10)]
 	public Animator playerAnim;
 	public float pAnimTime, pRandom;
 	int pAnim = 0;
+
 
 	float cTime, sTime, rTime;
 	// Use this for initialization
@@ -22,7 +35,14 @@ public class VideoControl : MonoBehaviour {
 		if (cTime - sTime >= pAnimTime + rTime){
 			pAnim = Random.Range(0,3);
 			playerAnim.SetInteger("anim",pAnim);
-			Debug.Log("foi");
+		}
+
+		if (equips.Length > 0){
+			scenario.sprite = cams[equips[0].currentTier];
+			microphone.sprite = mics[equips[1].currentTier];
+		}else{
+			scenario.sprite = cams[tests[0]];
+			microphone.sprite = mics[tests[1]];
 		}
 	}
 }
