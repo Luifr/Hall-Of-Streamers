ba8251c6fa688a623365808779c09b22c30de7ce
Oliver <oliverbecker@usp.br>
2018-10-12 05:56:57 +0000

    Display do horário atual do jogo implementado
    Display com o dia e hora atual do jogo adicionados
    à tela principal e corrigido o bug do tempo muito
    rápido.

--- Assets/Scripts/Relogio.cs
+++ Assets/Scripts/Relogio.cs
@@ -2,17 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+using UnityEngine.UI;
 
 public class Relogio : MonoBehaviour {
 
 	private float timer;
 	public static int ticks;
-	private float secPerTick = 3;
+	private float secPerTick = 2;
 	private int ticksPerDay = 61;
 	public static DateTime data;
+	public Text dataText;
 
 	// Use this for initialization
 	void Start () {
+		dataText = GameObject.Find ("DataAtual").GetComponent<Text> ();
 		data = DateTime.Now;
 		data = data.Date + new TimeSpan(12,0,0);
 	}
@@ -20,11 +23,16 @@ public class Relogio : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		timer += Time.deltaTime;
-		data = data.AddDays(1/(float)(ticksPerDay*secPerTick));
 		if(timer >= secPerTick){
 			timer-=secPerTick;
 			ticks++;
+			data = data.AddDays(1/(float)(ticksPerDay*secPerTick));
 			//data.AddDays(1/ticksPerDay);
 		}
+		DisplayData ();
+	}
+
+	public void DisplayData () {
+		dataText.text = data.ToString("dd/MM/yy  HH:mm");
 	}
 }
